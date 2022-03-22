385760b63b38ee7187fc6a6092d480558b999e18
yPitcher <Davi_9567@hotmail.com>
2022-03-21 21:42:40 -0300

    Adding base project

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <http_client.h>
+#include <iostream>
+#include <thread>
+
+void thread01(){
+    std::cout << "sou a thread 1!" << std::endl;
+}
+
+void thread02(){
+    std::cout << "sou a thread 2 com o parametro: " << _par1 << "!" << std::endl;
+}
+
+int main(){
+
+
+
+
+
+    
+    return 0;
+}
